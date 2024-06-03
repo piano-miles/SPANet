78bc9d9460fd8a26bc151c7452403fb596e6e1f9
Miles Waugh <miles.m.waugh@gmail.com>
2024-06-03 12:08:46 -0700

    Update export.py
    - Whitespace
    - Use list() instead of unnecessary list comprehension
    - Long collections are more easily read separated by lines
    - Use generator extension instead of for loops with appends
    - Use consistent single/double quote pattern

--- spanet/export.py
+++ spanet/export.py
@@ -14,11 +14,11 @@ from spanet.evaluation import load_model
 
 class WrappedModel(pl.LightningModule):
     def __init__(
-            self,
-            model: JetReconstructionModel,
-            input_log_transform: bool = False,
-            output_log_transform: bool = False,
-            output_embeddings: bool = False
+        self,
+        model: JetReconstructionModel,
+        input_log_transform: bool = False,
+        output_log_transform: bool = False,
+        output_embeddings: bool = False,
     ):
         super(WrappedModel, self).__init__()
 
@@ -30,10 +30,19 @@ class WrappedModel(pl.LightningModule):
     def apply_input_log_transform(self, sources):
         new_sources = []
         for (data, mask), name in zip(sources, self.model.event_info.input_names):
-            new_data = torch.stack([
-                mask * torch.log(data[:, :, i] + 1) if log_transformer else data[:, :, i]
-                for i, log_transformer in enumerate(self.model.event_info.log_features(name))
-            ], -1)
+            new_data = torch.stack(
+                [
+                    (
+                        mask * torch.log(data[:, :, i] + 1)
+                        if log_transformer
+                        else data[:, :, i]
+                    )
+                    for i, log_transformer in enumerate(
+                        self.model.event_info.log_features(name)
+                    )
+                ],
+                -1,
+            )
 
             new_sources.append(Source(new_data, mask))
         return new_sources
@@ -45,7 +54,7 @@ class WrappedModel(pl.LightningModule):
         outputs = self.model(sources)
 
         if self.output_log_transform:
-            assignments = [assignment for assignment in outputs.assignments]
+            assignments = list(outputs.assignments)
             detections = [F.logsigmoid(detection) for detection in outputs.detections]
 
             classifications = [
@@ -67,12 +76,22 @@ class WrappedModel(pl.LightningModule):
             for key in self.model.training_dataset.regressions.keys()
         ]
 
-        embedding_vectors = list(outputs.vectors.values()) if self.output_embeddings else []
+        embedding_vectors = (
+            list(outputs.vectors.values()) if self.output_embeddings else []
+        )
 
-        return *assignments, *detections, *regressions, *classifications, *embedding_vectors
+        return (
+            *assignments,
+            *detections,
+            *regressions,
+            *classifications,
+            *embedding_vectors,
+        )
 
 
-def onnx_specification(model, output_log_transform: bool = False, output_embeddings: bool = False):
+def onnx_specification(
+    model, output_log_transform: bool = False, output_embeddings: bool = False
+):
     input_names = []
     output_names = []
 
@@ -82,10 +101,7 @@ def onnx_specification(model, output_log_transform: bool = False, output_embeddi
         for input_type in ["data", "mask"]:
             current_input = f"{input_name}_{input_type}"
             input_names.append(current_input)
-            dynamic_axes[current_input] = {
-                0: 'batch_size',
-                1: f'num_{input_name}'
-            }
+            dynamic_axes[current_input] = {0: "batch_size", 1: f"num_{input_name}"}
 
     for output_name in model.event_info.event_particles.names:
         if output_log_transform:
@@ -99,11 +115,8 @@ def onnx_specification(model, output_log_transform: bool = False, output_embeddi
         else:
             output_names.append(f"{output_name}_detection_probability")
 
-    for regression in model.training_dataset.regressions.keys():
-        output_names.append(regression)
-
-    for classification in model.training_dataset.classifications.keys():
-        output_names.append(classification)
+    output_names.extend(iter(model.training_dataset.regressions.keys()))
+    output_names.extend(iter(model.training_dataset.classifications.keys()))
 
     if output_embeddings:
         output_names.append("EVENT/embedding_vector")
@@ -111,35 +124,41 @@ def onnx_specification(model, output_log_transform: bool = False, output_embeddi
         for particle, products in model.event_info.product_particles.items():
             output_names.append(f"{particle}/PARTICLE/embedding_vector")
 
-            for product in products:
-                output_names.append(f"{particle}/{product}/embedding_vector")
-
+            output_names.extend(
+                f"{particle}/{product}/embedding_vector" for product in products
+            )
     return input_names, output_names, dynamic_axes
 
 
 def main(
-        log_directory: str,
-        output_file: str,
-        input_log_transform: bool,
-        output_log_transform: bool,
-        output_embeddings: bool,
-        gpu: bool,
-        opset: int
+    log_directory: str,
+    output_file: str,
+    input_log_transform: bool,
+    output_log_transform: bool,
+    output_embeddings: bool,
+    gpu: bool,
+    opset: int,
 ):
     major_version, minor_version, *_ = torch.__version__.split(".")
     if int(major_version) == 2 and int(minor_version) == 0:
-        raise RuntimeError("ONNX export with Torch 2.0.x is not working. Either install 2.1 or 1.13.")
+        raise RuntimeError(
+            "ONNX export with Torch 2.0.x is not working. Either install 2.1 or 1.13."
+        )
 
     model = load_model(log_directory, cuda=gpu)
 
     # Create wrapped model with flat inputs and outputs
-    wrapped_model = WrappedModel(model, input_log_transform, output_log_transform, output_embeddings)
+    wrapped_model = WrappedModel(
+        model, input_log_transform, output_log_transform, output_embeddings
+    )
     wrapped_model.to(model.device)
     wrapped_model.eval()
     for parameter in wrapped_model.parameters():
         parameter.requires_grad_(False)
 
-    input_names, output_names, dynamic_axes = onnx_specification(model, output_log_transform, output_embeddings)
+    input_names, output_names, dynamic_axes = onnx_specification(
+        model, output_log_transform, output_embeddings
+    )
 
     batch = next(iter(model.train_dataloader()))
     sources = batch.sources
@@ -150,41 +169,61 @@ def main(
     print("-" * 60)
     print(f"Compiling network to ONNX model: {output_file}")
     if not input_log_transform:
-        print("WARNING -- No input log transform! User must apply log transform manually. -- WARNING")
+        print(
+            "WARNING -- No input log transform! User must apply log transform manually. -- WARNING"
+        )
     print("-" * 60)
-    
+
     wrapped_model.to_onnx(
         output_file,
         sources,
         input_names=input_names,
         output_names=output_names,
         dynamic_axes=dynamic_axes,
-        opset_version=opset
+        opset_version=opset,
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("log_directory", type=str,
-                        help="Pytorch Lightning Log directory containing the checkpoint and options file.")
+    parser.add_argument(
+        "log_directory",
+        type=str,
+        help="Pytorch Lightning Log directory containing the checkpoint and options file.",
+    )
 
-    parser.add_argument("output_file", type=str,
-                        help="Name to output the ONNX model to.")
+    parser.add_argument(
+        "output_file", type=str, help="Name to output the ONNX model to."
+    )
 
-    parser.add_argument("-g", "--gpu", action="store_true",
-                        help="Trace the network on a gpu.")
+    parser.add_argument(
+        "-g", "--gpu", action="store_true", help="Trace the network on a gpu."
+    )
 
-    parser.add_argument("--opset", type=int, default=15,
-                        help="ONNX opset version to use. Needs to be >= 14 for SPANet")
-    
-    parser.add_argument("--input-log-transform", action="store_true",
-                        help="Exported model will apply log transformations to input features automatically.")
+    parser.add_argument(
+        "--opset",
+        type=int,
+        default=15,
+        help="ONNX opset version to use. Needs to be >= 14 for SPANet",
+    )
 
-    parser.add_argument("--output-log-transform", action="store_true",
-                        help="Exported model will output log probabilities. This is more numerically stable.")
+    parser.add_argument(
+        "--input-log-transform",
+        action="store_true",
+        help="Exported model will apply log transformations to input features automatically.",
+    )
 
-    parser.add_argument("--output-embeddings", action="store_true",
-                        help="Exported model will also output the embeddings for every part of the event.")
+    parser.add_argument(
+        "--output-log-transform",
+        action="store_true",
+        help="Exported model will output log probabilities. This is more numerically stable.",
+    )
+
+    parser.add_argument(
+        "--output-embeddings",
+        action="store_true",
+        help="Exported model will also output the embeddings for every part of the event.",
+    )
 
     arguments = parser.parse_args()
     main(**arguments.__dict__)
