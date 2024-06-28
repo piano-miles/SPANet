3277a28114d8c684ce96fded1a2f42fb26992713
Miles Waugh <miles.m.waugh@gmail.com>
2024-06-28 13:42:15 +0200

    Delete scripts directory

--- scripts/wprime.sh
+++
@@ -1,3 +0,0 @@
-python -m spanet.train -of options_files/wprime/wprime.json --gpus 1 &&
-    python -m spanet.test ./spanet_output/version_0 -tf data/wprime/wprime_1000.h5 --gpu &&
-    python -m spanet.predict ./spanet_output/version_0 ./wprime_testing_output.h5 -tf data/wprime/wprime_1000.h5 --gpu
